47b4eed4eedeb7276488f5ddaaefe053395f590e
peastar1210 <josephcollins1210@gmail.com>
2024-07-09 16:53:11 -0400

    update: trading table style updated

--- components/staking/components/shared/TradingTable.tsx
+++ components/staking/components/shared/TradingTable.tsx
@@ -129,6 +129,7 @@ export default function TradingTable(props: any) {
 							height: "3px",
 							border: "none",
 							width: "3px",
+							overscrollBehaviorBlock: "true",
 						},
 						"&::-webkit-scrollbar-thumb": {
 							background: "gray",
@@ -140,7 +141,6 @@ export default function TradingTable(props: any) {
 							// borderRadius: '2px',
 							border: "none",
 						},
-						"&::-webkit-touch-callout": "none",
 					}}
 					className="rounded-[5px] p-[0px]">
 					<Table
